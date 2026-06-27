7f19783d0f0c3d53234f4e33683ee7f93413ec53
05Hilal05 <malikhilalahmad55@gmail.com>
2026-06-27 21:19:54 +0500

    Implement user account management features: change password, update details, and manage avatars

---
+++ src/models/subscriptions.model.js
@@ -0,0 +1,14 @@
+import mongoose, {Schema} from mongoose;
+
+const subscriptionSchema = new Schema({
+    subscriber:{
+        type: Schema.Types.ObjectId, //the one who is subscript
+        ref: "User"
+    },
+    channel: {
+        type: Schema.Types.ObjectId, // the channel which the user will subscribe channel is also user
+        ref: "User"
+    }
+},{timestamps: true})
+
+export const Subscription = mongoose.model("Subscription", subscriptionSchema)
